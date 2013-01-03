d9b809065ecb90532026aeabca25103657e752a1
Will Szumski <will@cowboycoders.org>
2013-01-03 03:01:11 +0000

    Add back striped copyright notices

--- formicidae_core/src/main/java/com/dsi/ant/AntMesg.java
+++ formicidae_core/src/main/java/com/dsi/ant/AntMesg.java
@@ -16,6 +16,22 @@
  *     You should have received a copy of the GNU General Public License
  *     along with formicidae.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/*
+ * Copyright 2010 Dynastream Innovations Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.dsi.ant;
 
 /**
