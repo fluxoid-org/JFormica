83f66ffa17094cbf6de74f076bce094112856d35
Will Szumski <will@cowboycoders.org>
2013-02-11 15:24:27 +0000

    Jars

---
+++ Cyclismo/SignalStrengthSampler/gen/org/cowboycoders/cyclisimo/signalstrength/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.cowboycoders.cyclisimo.signalstrength;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
